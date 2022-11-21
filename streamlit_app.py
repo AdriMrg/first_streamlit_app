d77838350048dbd4842e7fa164cb35c9c3b8f424
AdriMrg <118725318+AdriMrg@users.noreply.github.com>
2022-11-21 11:39:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Menu')
+streamlit.text('Un truc pas gras')
+streamlit.text('Un truc gras')
+streamlit.text('Un truc')
