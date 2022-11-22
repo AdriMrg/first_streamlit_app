bd3fa96524354cd96360502176252249399359e9
AdriMrg <118725318+AdriMrg@users.noreply.github.com>
2022-11-22 10:52:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Formate le json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Affiche le Json dans un joli tableau
+streamlit.dataframe(fruityvice_normalized)
